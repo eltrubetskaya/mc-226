8afaed630d33d4b5bbbfd80a94d28e79851052ab
Elena Trubetskaia <el.trubetskaya@gmail.com>
2019-01-31 12:19:02 +0200

    fixed collection

--- app/code/ElTrubetskaia/AskQuestion/Model/ResourceModel/AskQuestion/Collection.php
+++ app/code/ElTrubetskaia/AskQuestion/Model/ResourceModel/AskQuestion/Collection.php
@@ -7,6 +7,11 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 class Collection extends AbstractCollection
 {
     /**
+     * @var string
+     */
+    protected $_idFieldName = 'question_id';
+
+    /**
      * @var \Magento\Store\Model\StoreManagerInterface
      */
     private $storeManager;
