b7b7c99834bd75e213229c88143d7c172eba843f
Elena Trubetskaia <el.trubetskaya@gmail.com>
2019-03-22 17:14:42 +0200

    add custom attribute

--- app/code/ElTrubetskaia/AskQuestion/view/frontend/requirejs-config.js
+++ app/code/ElTrubetskaia/AskQuestion/view/frontend/requirejs-config.js
@@ -4,5 +4,12 @@ var config = {
             askQuestionFormValidator: 'ElTrubetskaia_AskQuestion/js/form-custom-validator',
             askQuestion: 'ElTrubetskaia_AskQuestion/js/ask_question'
         }
+    },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/set-shipping-information': {
+                'ElTrubetskaia_AskQuestion/js/action/set-shipping-information-mixin': true
+            }
+        }
     }
 };
